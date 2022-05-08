cd00a090c73bc3a004085825797fbdc7b99e9e1d
Orochi <orochi705@gmail.ru>
2022-05-08 09:11:07 +0900

    push

---
+++ Task_4.29/Program.cs
@@ -0,0 +1,12 @@
+﻿Random rnd_1 = new Random();
+Byte[] b = new Byte[5];
+rnd_1.NextBytes(b);
+Console.WriteLine(" ");
+for (int i = 0; i <= b.GetUpperBound(0); i++)
+    Console.Write("{1} ", i, b[i]);
+Random rnd_2 = new Random();
+Byte[] d = new Byte[3];
+rnd_2.NextBytes(d);
+Console.WriteLine(" ");
+for (int i = 0; i <= d.GetUpperBound(0); i++)
+    Console.Write("{1} ", i, d[i]);
